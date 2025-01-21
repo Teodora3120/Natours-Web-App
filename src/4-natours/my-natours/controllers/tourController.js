97cd26eff2c5de968c6298f8785efba91c5d7956
Teodora <vestale.teodora00@gmail.com>
2025-01-21 17:36:18 +0200

    fix: db name added in db url

--- src/4-natours/my-natours/controllers/tourController.js
+++ src/4-natours/my-natours/controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'Fail',
-      message: 'Missing name or price!',
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -29,12 +19,13 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
   console.log(req.body);
+  const newTour = await Tour.create(req.body);
   res.status(201).json({
     message: 'success',
     data: {
-      tour: '',
+      tour: newTour,
     },
   });
 };
