24c7673ca804d4994fd575f0ab111ecad3b1a23a
Teodora <vestale.teodora00@gmail.com>
2025-02-06 18:18:46 +0200

    feat: backend security against non sql injection, xss, parameter pollution, set security headers

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const morgan = require('morgan');
 const express = require('express');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const errorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -10,11 +15,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // GLOBAL MIDDLEWARES
-app.use(express.json()); // to be able to access the req.body
+
+// Set security http headers
+app.use(helmet());
+
+// development api call logs
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from the same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 1000 * 60 * 60,
@@ -22,8 +32,30 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
+// Body parser (reading  data from the body into req.body)
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NO SQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xssClean());
+// It prevents parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
