d9eaf24e8c0fbd7a1c11086242eff5276ecb9576
Teodora <vestale.teodora00@gmail.com>
2025-02-13 16:45:43 +0200

    feat: import users and reviews, indexes and calculate avg ratings

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,8 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+const dotenv = require('dotenv');
 const fs = require('fs');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: '../../config.env' });
 
@@ -21,9 +23,14 @@ mongoose
   });
 
 const tours = JSON.parse(fs.readFileSync('./tours.json', 'utf-8'));
+const users = JSON.parse(fs.readFileSync('./users.json', 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync('./reviews.json', 'utf-8'));
+
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data succesfully deleted!');
   } catch (err) {
     console.log(err);
@@ -35,6 +42,8 @@ const deleteData = async () => {
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data succesfully imported!');
   } catch (err) {
     console.log(err);
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -43,6 +44,49 @@ reviewSchema.pre(/^find/, function (next) {
     // })
     .select('-__v');
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log('Stats', stats);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats.nRating,
+      ratingsAverage: stats.avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  // this points to the current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.rev = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne() doesn't work here, the query has already executed
+  await this.rev.constructor.calcAverageRatings(this.rev.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
